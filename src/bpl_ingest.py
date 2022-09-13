1eeb22578fe318794a1cb0e25c6b0afa1682db3f
Tomek <klingaroo@gmail.com>
2022-09-13 08:56:55 -0400

    changes paths for using the script from the top level

--- src/bpl_ingest.py
+++ src/bpl_ingest.py
@@ -8,9 +8,9 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from bookops_marc import SierraBibReader
 
-from bpl_datastore import EnhancedBib
-from db_access import session_scope, insert_or_ignore
-from utils import save2csv, start_from_scratch
+from src.bpl_datastore import EnhancedBib
+from src.db_access import session_scope, insert_or_ignore
+from src.utils import save2csv, start_from_scratch
 
 
 def start_from_scratch(fh):
@@ -29,7 +29,7 @@ def ingest_cross_ref_data():
                 insert_or_ignore(session, EnhancedBib, bibNo=row[0], oclcNo=row[1])
 
 
-def select_for_sierra_list_creation(n=int) -> None:
+def select_for_sierra_list_creation(n=int) -> str:
     """
     Select next n number of identifiers for processing.
     Creates a csv file with sierra bib numbers to be used for list creation.
@@ -38,15 +38,15 @@ def select_for_sierra_list_creation(n=int) -> None:
         n:              number of identifiers to take
     """
     timestamp = datetime.now()
-    out_fh = f"./files/enhanced/BPL/batch-{timestamp:%y%m%d}-sierra-nos.csv"  # for archival purposes
+    out_fh = f"./src/files/enhanced/BPL/batch-{timestamp:%y%m%d}-sierra-nos.csv"  # for archival purposes
     temp_fh = os.path.join(
         os.getenv("USERPROFILE"),
-        f"documents/batch-{timestamp:%y%m%d}-sierra-nos.csv",
+        f"documents/bpl-batch2enrich-{timestamp:%y%m%d}-sierra-nos.csv",
     )
-    out_fh2 = f""
     start_from_scratch(out_fh)
+    start_from_scratch(temp_fh)
 
-    with session_scope("bpl_db.db") as session:
+    with session_scope("./src/bpl_db.db") as session:
         results = (
             session.query(EnhancedBib)
             .filter_by(enhanced=False)
@@ -58,17 +58,20 @@ def select_for_sierra_list_creation(n=int) -> None:
             save2csv(out_fh, [row.bibNo])
             save2csv(temp_fh, [f"b{row.bibNo}a"])
 
+    return temp_fh
+
 
 def parse_sierra_bib(src_fh: str = None) -> None:
     if src_fh is None:
         timestamp = datetime.now()
         src_fh = os.path.join(
-            os.getenv("USERPROFILE"), f"documents/batch2enhance-{timestamp:%y%m%d}.out"
+            os.getenv("USERPROFILE"),
+            f"documents/bpl-batch2enrich-{timestamp:%y%m%d}.out",
         )
     with open(src_fh, "rb") as marcfile:
         print(f"Reading {src_fh}.")
         reader = SierraBibReader(marcfile)
-        with session_scope("bpl_db.db") as session:
+        with session_scope("./src/bpl_db.db") as session:
             for bib in reader:
                 bibNo = bib.sierra_bib_id_normalized()
                 isbns = bib.get_fields("020")
